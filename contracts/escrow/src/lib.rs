55db2d57ca2c68b017edbdf525dab3ff751a266b
Karen Agbo <karenzitaagbo@gmail.com>
2026-07-07 14:54:30 +0100

    Update lib.rs

--- contracts/escrow/src/lib.rs
+++ contracts/escrow/src/lib.rs
@@ -1,8 +1,7 @@
 #![no_std]
 use soroban_sdk::{contract, contractimpl, Env, Symbol, Address, token};
-
 #[derive(soroban_sdk::ContractKls)]
-#[derive(Clone)]
+#[derive(Clone, Copy, Eq, PartialEq)]
 pub enum EscrowStatus {
     Created,
     Funded,
@@ -10,10 +9,8 @@ pub enum EscrowStatus {
     Completed,
     Disputed,
 }
-
 #[contract]
 pub struct Escrow;
-
 #[contractimpl]
 impl Escrow {
     pub fn init(env: Env, buyer: Address, seller: Address, amount: i128, treasury_address: Address) {
@@ -23,17 +20,13 @@ impl Escrow {
         env.storage().instance().set(&Symbol::new(&env, "treasury"), &treasury_address);
         env.storage().instance().set(&Symbol::new(&env, "status"), &EscrowStatus::Created);
     }
-
-    pub fn fund(env: Env, token: token::Client) {
+    pub fn fund(env: Env, token_address: Address) {
         let buyer: Address = env.storage().instance().get(&Symbol::new(&env, "buyer")).unwrap();
         let amount: i128 = env.storage().instance().get(&Symbol::new(&env, "amount")).unwrap();
-        
-        // Buyer transfers funds to the escrow contract
+        let token = token::Client::from_address(&env, &token_address);
         token.transfer(&buyer, &env.current_contract_address(), &amount);
-        
         env.storage().instance().set(&Symbol::new(&env, "status"), &EscrowStatus::Funded);
     }
-
     pub fn mark_shipped(env: Env) {
         let seller: Address = env.storage().instance().get(&Symbol::new(&env, "seller")).unwrap();
         if env.auth() != seller {
@@ -41,40 +34,30 @@ impl Escrow {
         }
         env.storage().instance().set(&Symbol::new(&env, "status"), &EscrowStatus::Shipped);
     }
-
-    pub fn release_funds(env: Env, token: token::Client) {
+    pub fn release_funds(env: Env, token_address: Address) {
         let buyer: Address = env.storage().instance().get(&Symbol::new(&env, "buyer")).unwrap();
         let seller: Address = env.storage().instance().get(&Symbol::new(&env, "seller")).unwrap();
         let amount: i128 = env.storage().instance().get(&Symbol::new(&env, "amount")).unwrap();
         let treasury: Address = env.storage().instance().get(&Symbol::new(&env, "treasury")).unwrap();
         let status: EscrowStatus = env.storage().instance().get(&Symbol::new(&env, "status")).unwrap();
-
         if env.auth() != buyer {
             panic!("Only the buyer can release funds");
         }
         if status != EscrowStatus::Shipped {
             panic!("Funds can only be released after shipping");
         }
-
-        // Calculate 1% fee for treasury
+        let token = token::Client::from_address(&env, &token_address);
         let fee = amount / 100;
         let seller_amount = amount - fee;
-
-        // 1. Send fee to Treasury Contract
-        // Inter-contract communication: we call a function on the treasury contract
         let treasury_client = soroban_sdk::contractclient!(Treasury);
         treasury_client.call(&env, &treasury, soroban_sdk::contractcall::Args {
             token: token.clone(),
             from: env.current_contract_address(),
             amount: fee,
         });
-
-        // 2. Send remaining funds to seller
         token.transfer(&env.current_contract_address(), &seller, &seller_amount);
-        
         env.storage().instance().set(&Symbol::new(&env, "status"), &EscrowStatus::Completed);
     }
-
     pub fn get_status(env: Env) -> EscrowStatus {
         env.storage().instance().get(&Symbol::new(&env, "status")).unwrap()
     }
